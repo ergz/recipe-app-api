58b621b62fef0104717f44002f0129fdfa1839b4
Emanuel Rodriguez <emnlrodriguez@gmail.com>
2022-08-14 22:54:01 -0700

    adds creating superuser

--- app/core/models.py
+++ app/core/models.py
@@ -15,12 +15,22 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("User must have an email address")
-            
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password=None):
+        """ Create and return a new superuser """
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True 
+        user.is_superuser = True 
+        user.save(using=self._db)
+
+        return user
+        
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,9 @@ class ModelTest(TestCase):
     def test_new_user_without_email_raises_error(self):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user("", "pass123")
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
